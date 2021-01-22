7759434b66043fac92fa3b0063709b7da57ccf08
HyeongLee <leeceo97@naver.com>
2021-01-23 06:47:35 +0900

    유저페이지 완성및 상품페이지구성중

--- fc_django/fc_django/urls.py
+++ fc_django/fc_django/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path,include
+from fcuser.views import index,RegisterView,LoginView
+from product.views import ProductList,ProductCreate
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',index),
+    #클래스같은경우는 뒤에 as_view()를붙여야함
+    path('register/',RegisterView.as_view()),
+    path('login/',LoginView.as_view()),
+    path('product/',ProductList.as_view()),
+    path('product/create/',ProductCreate.as_view()),
 ]
--- fc_django/fcuser/views.py
+++ fc_django/fcuser/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import FormView
+from .forms import RegisterForm,LoginForm
 # Create your views here.
+
+def index(request):
+    return render(request,'index.html',{ 'email':request.session.get('user') })
+
+class RegisterView(FormView):
+    template_name='register.html'
+    form_class=RegisterForm
+    success_url='/'
+
+class LoginView(FormView):
+    template_name='login.html'
+    form_class=LoginForm
+    success_url='/'
+
+# 데이터가 유효성검사가 끝났을때 들어오는 함수
+    def form_valid(self,form):
+        self.request.session['user']=form.email
+
+        return super().form_valid(form)
